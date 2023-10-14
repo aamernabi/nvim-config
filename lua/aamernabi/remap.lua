9b214bb1bbf29da61ff4ed3bd7c6f69bc29ba296
Aamer Paul <aamerpaul@gmail.com>
2023-10-14 15:06:06 +0530

    updated - key remaps

--- lua/aamernabi/remap.lua
+++ lua/aamernabi/remap.lua
@@ -1,2 +1,20 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+
+-- new changes -- adjust your settings
+vim.opt.backspace = '2'
+vim.opt.showcmd = true
+vim.opt.laststatus = 2
+vim.opt.autowrite = true
+vim.opt.cursorline = true
+vim.opt.autoread = true
+
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+
+vim.cmd [[ set noswapfile ]]
+
+vim.wo.number = true
