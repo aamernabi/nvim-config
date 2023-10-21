01be6453aca1516b120ebb569202e81c60fd873b
Aamer Paul <aamerpaul@gmail.com>
2023-10-21 13:01:24 +0530

    update - configured lsp - nvim-cmp

--- lua/core/packer.lua
+++ lua/core/packer.lua
@@ -4,15 +4,16 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
+  -- telescope
   use {
 	  'nvim-telescope/telescope.nvim', tag = '0.1.3',
 	  -- or                            , branch = '0.1.x',
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
 
+  -- rose-pine color scheme
   use({ 
 	  'rose-pine/neovim', 
 	  as = 'rose-pine',
@@ -21,7 +22,24 @@ return require('packer').startup(function(use)
 	  end
   })
 
+  -- treesitter and undotree
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
   use('nvim-treesitter/playground')
   use('mbbill/undotree')
+
+  -- lsp
+  use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'neovim/nvim-lspconfig'
+  use 'hrsh7th/cmp-cmdline'
+
+  use 'williamboman/mason.nvim'
+  use 'williamboman/mason-lspconfig.nvim'
+
+  use 'L3MON4D3/LuaSnip'
+  use 'saadparwaiz1/cmp_luasnip'
+  use 'rafamadriz/friendly-snippets'
+
 end)
