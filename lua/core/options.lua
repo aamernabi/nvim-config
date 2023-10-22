bc1895c273b5bcf9cc3a6517922c94ca75961777
Aamer Paul <aamerpaul@gmail.com>
2023-10-22 12:15:40 +0530

    update - add keymaps

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -11,7 +11,9 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.shiftround = true
 vim.opt.expandtab = true
+vim.opt.clipboard = 'unnamedplus'
 
 vim.cmd [[ set noswapfile ]]
 
 vim.wo.number = true
+
