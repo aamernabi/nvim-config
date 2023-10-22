439623c97241d9c3703ff798da16b0673ad0bc99
Aamer Paul <aamerpaul@gmail.com>
2023-10-22 22:07:57 +0530

    update - keymaps

--- after/plugin/nvim-cmp.lua
+++ after/plugin/nvim-cmp.lua
@@ -1,6 +1,8 @@
 local cmp = require('cmp')
 -- local cmp_select = { behaviour = cmp.SelectBehaviour.Select }
 
+require("luasnip/loaders/from_vscode").lazy_load()
+
 cmp.setup({
   snippet = {
     expand = function(args)
--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,3 +1,4 @@
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("n", "<C-a>", 'ggVG')
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+vim.keymap.set('n', '<C-a>', 'ggVG')
+vim.keymap.set('n', 'dw', 'vb"_d')
 
