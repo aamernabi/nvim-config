bc2ef28d84f5b55be4b8e2801a67d8ab5a48f508
Aamer Paul <aamerpaul@gmail.com>
2023-10-28 21:00:04 +0530

    update - new theme and added lualine

---
+++ after/plugin/color-scheme.lua
@@ -0,0 +1,3 @@
+vim.o.background = "dark" -- or "light" for light mode
+vim.o.termguicolors = true -- enable True Color (24-bit) support for the terminal
+vim.cmd([[colorscheme gruvbox]])
--- lua/core/packer.lua
+++ lua/core/packer.lua
@@ -1,26 +1,31 @@
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
 
--- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+local packer_bootstrap = ensure_packer()
+
 
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
   -- telescope
   use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.3',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
+    'nvim-telescope/telescope.nvim', tag = '0.1.4',
+    -- or                            , branch = '0.1.x',
+    requires = { {'nvim-lua/plenary.nvim'} }
   }
 
-  -- rose-pine color scheme
-  use({ 
-	  'rose-pine/neovim', 
-	  as = 'rose-pine',
-	  config = function()
-		  vim.cmd('colorscheme rose-pine')
-	  end
-  })
+  -- color scheme
+  use 'ellisonleao/gruvbox.nvim'
+  use 'nvim-lualine/lualine.nvim'
+  use 'nvim-tree/nvim-web-devicons'
 
   -- treesitter and undotree
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
@@ -44,4 +49,10 @@ return require('packer').startup(function(use)
   use 'saadparwaiz1/cmp_luasnip'
   use 'rafamadriz/friendly-snippets'
 
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
