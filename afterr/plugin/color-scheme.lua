21c44182eef3534768b7628ed9304064c284845e
Aamer Paul <aamerpaul@gmail.com>
2023-11-23 16:27:24 +0530

    update - code optimization lazy.nvim:q

--- afterr/plugin/color-scheme.lua
+++
@@ -1,7 +0,0 @@
-local opt = vim.o
-opt.background = "dark" -- or "light" for light mode
-opt.termguicolors = true -- enable True Color (24-bit) support for the terminal
-opt.guifont = 'JetBrainsMono Nerd Font'
-
-vim.cmd([[colorscheme gruvbox]])
-
