1cfd41afa029aadc5831fe582457e197827ff0ac
Aamer Paul <aamerpaul@gmail.com>
2024-02-13 22:27:44 +0530

    update - open buffer keymap

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -27,7 +27,7 @@ return {
 
     keymap.set('n', '<leader>ff', builtin.find_files, {})
     keymap.set('n', '<C-p>', builtin.git_files, {})
-    keymap.set('n', '<C-b>', builtin.buffers, {})
+    keymap.set('n', '<leader>fb', builtin.buffers, {})
     keymap.set('n', '<leader>fh', builtin.help_tags, {})
     keymap.set('n', '<leader>fg', builtin.live_grep, {})
     keymap.set('n', '<leader>fh', builtin.help_tags, {})
