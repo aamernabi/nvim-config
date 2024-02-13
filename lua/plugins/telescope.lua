1faa161868ff2e9b019617ad471de925dcd15e4e
Aamer Paul <aamerpaul@gmail.com>
2024-02-13 21:49:52 +0530

    added - navigation mapping keys in telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,18 +5,36 @@ return {
   dependencies = {
     'nvim-lua/plenary.nvim',
     'nvim-tree/nvim-web-devicons',
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
   },
   config = function()
+    local telescope = require('telescope')
     local builtin = require('telescope.builtin')
+    local actions = require('telescope.actions')
     local keymap = vim.keymap
+
+    telescope.setup({
+      defaults = {
+        path_display = { 'truncate ' },
+        mappings = {
+          i = {
+            ['<C-k>'] = actions.move_selection_previous,
+            ['<C-j>'] = actions.move_selection_next,
+          },
+        },
+      },
+    })
 
     keymap.set('n', '<leader>ff', builtin.find_files, {})
     keymap.set('n', '<C-p>', builtin.git_files, {})
     keymap.set('n', '<C-b>', builtin.buffers, {})
     keymap.set('n', '<leader>fh', builtin.help_tags, {})
     keymap.set('n', '<leader>fg', builtin.live_grep, {})
+    keymap.set('n', '<leader>fh', builtin.help_tags, {})
     keymap.set('n', '<leader>fs', function()
       builtin.grep_string({ search = vim.fn.input('Grep > ') })
     end)
+
+    telescope.load_extension('fzf')
   end,
 }
