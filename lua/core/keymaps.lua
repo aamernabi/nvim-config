7232d5abb00905794d00c22d702f3328f76d94ef
Aamer Paul <aamerpaul@gmail.com>
2024-02-13 22:15:28 +0530

    update - added move highlighted code keymap

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,4 +1,4 @@
-vim.g.mapleader = " "
+vim.g.mapleader = ' '
 
 local keymap = vim.keymap
 
@@ -7,6 +7,9 @@ keymap.set('n', '<C-a>', 'ggVG') -- select all
 keymap.set('n', '<leader>nh', ':nohl<CR>') -- hide search highlight
 keymap.set('n', 'x', '"_x') -- don't copy char when using x
 keymap.set('n', '<leader>xx', '"_dd') -- delete line and don't copy
+
+keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
 
 -- split window
 keymap.set('n', '<leader>sv', '<C-w>v') -- vertical
@@ -17,9 +20,8 @@ keymap.set('n', '<leader>se', '<C-w>=') -- equal width
 -- tabs
 keymap.set('n', '<leader>to', ':tabnew<CR>')
 keymap.set('n', '<leader>tx', ':tabclose<CR>')
-keymap.set('n', '<leader>tn', ':tabn<CR>') -- next tab 
+keymap.set('n', '<leader>tn', ':tabn<CR>') -- next tab
 keymap.set('n', '<leader>tp', ':tabp<CR>') -- previous tab
 
 -- buffers
 keymap.set('n', '<leader>bx', ':bd<CR>') -- close buffer
-
