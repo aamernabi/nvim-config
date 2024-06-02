73379c8882bab700bba040b06cbb441882d6dcab
Aamer Paul <aamerpaul@gmail.com>
2024-06-02 18:15:54 +0530

    fixed jdtls keymap issue

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -69,24 +69,21 @@ local config = {
 }
 require('jdtls').start_or_attach(config)
 
-vim.keymap.set('n', '<leader>co', "<Cmd>lua require'jdtls'.organize_imports()<CR>", { desc = 'Organize Imports' })
-vim.keymap.set('n', '<leader>crv', "<Cmd>lua require('jdtls').extract_variable()<CR>", { desc = 'Extract Variable' })
-vim.keymap.set(
+local keymap = vim.keymap
+
+keymap.set('n', '<leader>co', "<Cmd>lua require('jdtls').organize_imports()<CR>", { desc = 'Organize Imports' })
+keymap.set('n', '<leader>crv', "<Cmd>lua require('jdtls').extract_variable()<CR>", { desc = 'Extract Variable' })
+keymap.set('n', '<leader>crc', "<Cmd>lua require('jdtls').extract_constant()<CR>", { desc = 'Extract Constant' })
+keymap.set(
   'v',
   '<leader>crv',
   "<Esc><Cmd>lua require('jdtls').extract_variable(true)<CR>",
   { desc = 'Extract Variable' }
 )
-vim.keymap.set('n', '<leader>crc', "<Cmd>lua require('jdtls').extract_constant()<CR>", { desc = 'Extract Constant' })
-vim.keymap.set(
+keymap.set(
   'v',
   '<leader>crc',
   "<Esc><Cmd>lua require('jdtls').extract_constant(true)<CR>",
   { desc = 'Extract Constant' }
 )
-vim.keymap.set(
-  'v',
-  '<leader>crm',
-  "<Esc><Cmd>lua require('jdtls').extract_method(true)<CR>",
-  { desc = 'Extract Method' }
-)
+keymap.set('v', '<leader>crm', "<Esc><Cmd>lua require('jdtls').extract_method(true)<CR>", { desc = 'Extract Method' })
